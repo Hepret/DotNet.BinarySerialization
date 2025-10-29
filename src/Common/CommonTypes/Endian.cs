1c7a18887c26c38551829142e97a523693c5674f
Saulin Daniel <saulin.daniel@gmail.com>
2025-10-29 18:08:29 +0300

    Improve: Solution structure Add: Test and Common solution folders Add: CommonTypes project

---
+++ src/Common/CommonTypes/Endian.cs
@@ -0,0 +1,17 @@
+﻿namespace CommonTypes;
+
+/// <summary>
+/// Specifies the byte order (endianness) used when representing multi-byte values in memory or during data transmission.
+/// </summary>
+public enum Endian
+{
+    /// <summary>
+    /// Big-endian — the most significant byte is stored at the lowest memory address (order from most significant to least significant).
+    /// </summary>
+    Big = 0,
+
+    /// <summary>
+    /// Little-endian — the least significant byte is stored at the lowest memory address (order from least significant to most significant).
+    /// </summary>
+    Little = 1
+}
